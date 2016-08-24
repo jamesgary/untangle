76df5a45c98cf2243e88618c12d7c317c7f5e8bf
James Gary <james.gary@sendgrid.com>
2016-08-23 23:15:14 -0700

    Poor Man's FSM

--- js/game.js
+++ js/game.js
@@ -33,6 +33,15 @@ let Q = {
   edgeColor: '#0057d4',
 };
 
+let DEFAULT_STATE       = "default";
+let HOVER_STATE         = "hover";
+let DRAG_STATE          = "drag";
+let SELECT_OPEN_STATE   = "select_open";
+let SELECT_CLOSED_STATE = "select_closed";
+let SELECT_DRAG_STATE   = "select_drag";
+
+let currentState = DEFAULT_STATE;
+
 module.exports = class Game {
   constructor(canvas) {
     let edges = GraphGenerators.generateTantalo({numLines: Q.nodeCount});
@@ -105,6 +114,8 @@ module.exports = class Game {
 
   draw(ticksPassed) {
     if (this.is_dirty) {
+      $("#state").text(currentState);
+
       // resize and clear canvas
       this.canvas.width  = window.innerWidth;
       this.canvas.height = window.innerHeight;
@@ -136,7 +147,7 @@ module.exports = class Game {
         } else if (node == this.draggedNode) { // draw same as hovered
           this.ctx.fillStyle = Q.nodeHoverFill;
           this.ctx.strokeStyle = Q.nodeHoverOutline;
-        } else if (!this.selectedNodes && node.isNeighboring) { // only show neighboring if not lasso'd
+        } else if (currentState != SELECT_DRAG_STATE && node.isNeighboring) { // only show neighboring if not lasso'd
           this.ctx.fillStyle = Q.nodeNeighborFill;
           this.ctx.strokeStyle = Q.nodeNeighborOutline;
         } else if (node == this.hoveredNode) {
@@ -177,66 +188,67 @@ module.exports = class Game {
     let rect = canvas.getBoundingClientRect();
     let x = evt.clientX - rect.left;
     let y = evt.clientY - rect.top;
+    let xDistToMove;
+    let yDistToMove;
+    let newHoveredNode = this.getTopTouchingNode(x, y);
 
     if (this.draggedNode) {
-      let xDistToMove = x - this.draggedNode.x + this.draggedNodeXOffset;
-      let yDistToMove = y - this.draggedNode.y + this.draggedNodeYOffset;
+      xDistToMove = x - this.draggedNode.x + this.draggedNodeXOffset;
+      yDistToMove = y - this.draggedNode.y + this.draggedNodeYOffset;
+    }
 
-      if (this.draggedNode.isBeingSelected) {
-        // drag all its buddies
+    switch (currentState) {
+      case DEFAULT_STATE:
+        if (newHoveredNode) {
+          currentState = HOVER_STATE;
+          this.$canvas.css("cursor", "pointer");
+        }
+        break;
+      case HOVER_STATE:
+        if (newHoveredNode != this.hoveredNode) {
+          if (newHoveredNode == null) {
+            currentState = DEFAULT_STATE;
+          }
+        }
+        break;
+      case DRAG_STATE:
+        this.draggedNode.x += xDistToMove;
+        this.draggedNode.y += yDistToMove;
+        break;
+      case SELECT_OPEN_STATE:
+        this.lassoCorner2X = x;
+        this.lassoCorner2Y = y;
+
+        let width = Math.abs(this.lassoCorner1X - this.lassoCorner2X);
+        let height = Math.abs(this.lassoCorner1Y - this.lassoCorner2Y);
+        let centerLassoX = Math.abs(this.lassoCorner1X + this.lassoCorner2X) / 2;
+        let centerLassoY = Math.abs(this.lassoCorner1Y + this.lassoCorner2Y) / 2;
+
+        for (let node of this.graph.nodes) {
+          let distX = Math.abs(node.x - centerLassoX)
+          let distY = Math.abs(node.y - centerLassoY)
+
+          if (distX > (width  / 2 + Q.nodeRad)) { node.isBeingSelected = false; continue; }
+          if (distY > (height / 2 + Q.nodeRad)) { node.isBeingSelected = false; continue; }
+
+          if (distX <= (width  / 2) && distY <= (height / 2)) { node.isBeingSelected = true; continue; }
+
+          var dx = distX - width / 2;
+          var dy = distY - height / 2;
+          node.isBeingSelected = (dx*dx+dy*dy<=(Q.nodeRad*Q.nodeRad));
+        }
+        break;
+      case SELECT_CLOSED_STATE:
+        break;
+      case SELECT_DRAG_STATE:
         for (let node of this.selectedNodes) {
           node.x += xDistToMove;
           node.y += yDistToMove;
         }
-      } else {
-        // drag it!
-        this.draggedNode.x += xDistToMove;
-        this.draggedNode.y += yDistToMove;
-      }
+        break;
     }
 
-    if (this.lassoCorner1X) {
-      this.lassoCorner2X = x;
-      this.lassoCorner2Y = y;
-
-      let width = Math.abs(this.lassoCorner1X - this.lassoCorner2X);
-      let height = Math.abs(this.lassoCorner1Y - this.lassoCorner2Y);
-      let centerLassoX = Math.abs(this.lassoCorner1X + this.lassoCorner2X) / 2;
-      let centerLassoY = Math.abs(this.lassoCorner1Y + this.lassoCorner2Y) / 2;
-
-      for (let node of this.graph.nodes) {
-        let distX = Math.abs(node.x - centerLassoX)
-        let distY = Math.abs(node.y - centerLassoY)
-
-        if (distX > (width  / 2 + Q.nodeRad)) { node.isBeingSelected = false; continue; }
-        if (distY > (height / 2 + Q.nodeRad)) { node.isBeingSelected = false; continue; }
-
-        if (distX <= (width  / 2) && distY <= (height / 2)) { node.isBeingSelected = true; continue; }
-
-        var dx = distX - width / 2;
-        var dy = distY - height / 2;
-        node.isBeingSelected = (dx*dx+dy*dy<=(Q.nodeRad*Q.nodeRad));
-      }
-    } else {
-      let newHoveredNode = this.getTopTouchingNode(x, y);
-
-      if (newHoveredNode) {
-        if (newHoveredNode != this.hoveredNode) {
-          if (!this.hoveredNode) {
-            this.$canvas.css("cursor", "-webkit-grab");
-            this.$canvas.css("cursor", "-moz-grab");
-            this.$canvas.css("cursor", "grab");
-          }
-          this.hoveredNode = newHoveredNode;
-        }
-      } else {
-        if (newHoveredNode != this.hoveredNode) {
-          this.hoveredNode = null;
-          this.$canvas.css("cursor", "default");
-        }
-      }
-    }
-
+    this.hoveredNode = newHoveredNode;
     this.is_dirty = true;
   }
 
@@ -245,72 +257,87 @@ module.exports = class Game {
     let x = evt.clientX - rect.left;
     let y = evt.clientY - rect.top;
 
-    if (this.hoveredNode) {
-      this.draggedNode = this.hoveredNode;
-      this.draggedNodeXOffset = this.draggedNode.x - x;
-      this.draggedNodeYOffset = this.draggedNode.y - y;
+    switch (currentState) {
+      case DEFAULT_STATE:
+        // start crosshair to select
+        currentState = SELECT_OPEN_STATE;
+        this.lassoCorner1X = x;
+        this.lassoCorner1Y = y;
+        this.lassoCorner2X = x;
+        this.lassoCorner2Y = y;
+        break;
+      case HOVER_STATE:
+        currentState = DRAG_STATE;
+        this.draggedNode = this.hoveredNode;
+        this.draggedNodeXOffset = this.draggedNode.x - x;
+        this.draggedNodeYOffset = this.draggedNode.y - y;
 
-      this.$canvas.css("cursor", "-webkit-grabbing");
-      this.$canvas.css("cursor", "-moz-grabbing");
-      this.$canvas.css("cursor", "grabbing");
-
-      // eh, gotta modify the model
-      for (let node of this.draggedNode.nodes) {
-        node.isNeighboring = true;
-      }
-    } else {
-      this.$canvas.css("cursor", "crosshair");
-      this.lassoCorner1X = x;
-      this.lassoCorner1Y = y;
-      this.lassoCorner2X = x;
-      this.lassoCorner2Y = y;
-      this.mousemove(evt); // trigger a move
+        // eh, gotta modify the model
+        for (let node of this.draggedNode.nodes) {
+          node.isNeighboring = true;
+        }
+        break;
+      case SELECT_CLOSED_STATE:
+        if (this.hoveredNode && this.hoveredNode.isBeingSelected) {
+          currentState = SELECT_DRAG_STATE;
+          this.draggedNode = this.hoveredNode;
+          this.draggedNodeXOffset = this.draggedNode.x - x;
+          this.draggedNodeYOffset = this.draggedNode.y - y;
+        } else {
+          currentState = DEFAULT_STATE;
+          for (let node of this.selectedNodes) {
+            node.isBeingSelected = false;
+          }
+          this.selectedNodes = [];
+        }
+        break;
     }
+
+    this.mousemove(evt); // trigger a move
     this.is_dirty = true;
   }
 
   mouseup(evt) {
-    if (this.lassoCorner1X) { // just finished initial selection
-      this.lassoCorner1X = null;
-      this.lassoCorner1Y = null;
-      this.lassoCorner2X = null;
-      this.lassoCorner2Y = null;
-      this.selectedNodes = [];
-      for (let node of this.graph.nodes) {
-        if (node.isBeingSelected) {
-          this.selectedNodes.push(node);
-        }
-      }
-
-      this.mousemove(evt); // trigger a move
-    } else {
-      // clear the selection
-      if (this.selectedNodes.length > 0) {
-        for (let node of this.selectedNodes) {
-          node.isBeingSelected = false;
-        }
-        this.selectedNodes = [];
-      }
-      if (this.draggedNode) {
+    switch (currentState) {
+      case DRAG_STATE:
+        currentState = DEFAULT_STATE;
         for (let node of this.draggedNode.nodes) {
           node.isNeighboring = false;
         }
+        this.hoveredNode = null;
         this.draggedNode = null;
-      }
-    }
-
-    if (this.hoveredNode) {
-      this.$canvas.css("cursor", "-webkit-grab");
-      this.$canvas.css("cursor", "-moz-grab");
-      this.$canvas.css("cursor", "grab");
-    } else {
-      this.$canvas.css("cursor", "default");
+        break;
+      case SELECT_OPEN_STATE:
+        currentState = SELECT_CLOSED_STATE;
+        this.lassoCorner1X = null;
+        this.lassoCorner1Y = null;
+        this.lassoCorner2X = null;
+        this.lassoCorner2Y = null;
+        this.selectedNodes = [];
+        for (let node of this.graph.nodes) {
+          if (node.isBeingSelected) {
+            this.selectedNodes.push(node);
+          }
+        }
+        this.hoveredNode = null;
+        this.draggedNode = null;
+        break;
+      case SELECT_DRAG_STATE:
+        currentState = DEFAULT_STATE;
+        for (let node of this.selectedNodes) {
+          node.isBeingSelected = false;
+        }
+        this.hoveredNode = null;
+        this.draggedNode = null;
+        this.selectedNodes = [];
     }
 
     let intersectingEdges = this.graph.getIntersectingEdges();
     if (intersectingEdges.length == 0) {
-      console.log("YOU WIN");
+      alert("YOU WIN");
     }
+
+    this.mousemove(evt); // trigger a move
     this.is_dirty = true;
   }
 
